80965b2d86039c32d32c383b583c4e37e7b5d8c0
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-09-17 15:06:56 -0600

    Commit para subir cambios.

---
+++ Spring-Proyects/webproject/src/main/java/com/example/arthycode/springweb/webproject/WebprojectApplication.java
@@ -0,0 +1,14 @@
+package com.example.arthycode.springweb.webproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebprojectApplication.class, args);
+    System.out.println("Hola desde Spring");
+	}
+
+}
