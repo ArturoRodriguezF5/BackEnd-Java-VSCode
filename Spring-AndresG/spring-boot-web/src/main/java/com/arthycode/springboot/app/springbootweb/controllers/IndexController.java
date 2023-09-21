8d55e2a2afe3d77469c1b39067ddee6c5c61111e
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-09-21 11:22:57 -0600

    Commit para subir cambiso realizados

--- Spring-AndresG/spring-boot-web/src/main/java/com/arthycode/springboot/app/springbootweb/controllers/IndexController.java
+++ Spring-AndresG/spring-boot-web/src/main/java/com/arthycode/springboot/app/springbootweb/controllers/IndexController.java
@@ -1,13 +1,19 @@
 package com.arthycode.springboot.app.springbootweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+//import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
   
   @GetMapping({"/index", "/", "/home"})
-  public String index() {
+  public String index(Model model) {
+    // Por detras la interfaz ModelMap(Llave valor), para pasar atributos a la vista(plantilla.html)
+    model.addAttribute("titulo", "Hola spring framework con Model");
     return "index";
   }
 
