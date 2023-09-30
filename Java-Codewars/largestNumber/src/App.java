48ced361cbdf5277dec17449ebcd8b8cbe909195
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-09-29 20:33:36 -0600

    Commit para subir ejercicio de largestnumber.

---
+++ Java-Codewars/largestNumber/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+  public static void main(String[] args) throws Exception {
+    String cad = "gh12cdy695m1";
+    System.out.println(largestNumber(cad));
+  }
+
+  public static int largestNumber(String cadena) {
+    int myInt = 5;
+    System.out.println(((Object) cadena.charAt(2)).getClass().getSimpleName());
+    return 0;
+  }
+}
