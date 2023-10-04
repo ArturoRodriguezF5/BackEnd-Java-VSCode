c9bfb0db7bbcd940b60336cd74cacc3600ecff04
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-10-03 23:31:15 -0600

    Commit para actualizar cambios.

--- Java-Codewars/largestNumber/src/App.java
+++ Java-Codewars/largestNumber/src/App.java
@@ -9,6 +9,14 @@ public class App {
     System.out.println(((Object) cadena.charAt(2)).getClass().getSimpleName());
     System.out.println(((Object) myInt).getClass().getSimpleName());
     System.out.println("Buscar como separar la cadena en numeros y letras");
+    char[] cad = cadena.toCharArray();
+    String n = "";
+    for(int i = 0; i < cad.length; i++) {
+      if(Character.isDigit(cad[i])) {
+        n += cad[i];
+      }
+    }
+    System.out.println(n);
     return 0;
   }
 }
